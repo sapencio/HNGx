f0eadccf2536e72ff4a8ae17347e1b982d6a4fde
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2023-09-08 23:32:08 +0300

    Update github url

---
+++ stage 1/script.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from datetime import datetime, timezone
+import requests
+
+app = Flask(__name__)
+response = requests.get()
+
+@app.route('/')
+def hello(slack_name: str, track: str = None):
+    return {
+        "slack_name": "Tibebe",
+        "current_date": datetime.now().strftime("%A"),
+        "utc_date": datetime.now(timezone.utc),
+        "track": "Backend",
+        "github_file_url": "https://github.com/sapencio/HNGx/blob/master/stage%201/script.py",
+        "github_repo_url": "https://github.com/sapencio/HNGx/tree/master",
+        "status_code": response.status_code
+    }
