160bf1d976fd2b121817e14e10a7bc7b31a6fbad
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2023-09-09 23:38:15 +0300

    New commit

--- stage 1/README.md
+++ stage 1/README.md
@@ -1,1 +1,2 @@
 # Stage 1
+Create and host an endpoint using any programming language of your choice. The endpoint should take two GET request query parameters and return specific information in JSON format.
--- stage 1/app.py
+++ stage 1/app.py
@@ -1,16 +1,34 @@
-from flask import Flask
-from datetime import datetime, timezone
+from flask import Flask, request, jsonify
+import datetime
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello(slack_name: str, track: str = None):
-    return {
-        "slack_name": slack_name,
-        "current_date": datetime.now().strftime("%A"),
-        "utc_date": datetime.now(timezone.utc),
-        "track": track,
-        "github_file_url": "https://github.com/sapencio/HNGx/blob/master/stage%201/app.py",
-        "github_repo_url": "https://github.com/sapencio/HNGx/tree/master",
-        "status_code": 200,
-    }
+
+@app.route('/get_info', methods=['GET'])
+def get_info():
+    try:
+        slack_name = request.args.get('slack_name').title()
+        track = request.args.get('track').title()
+        current_day = datetime.datetime.now().strftime('%A')
+        utc_time = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+
+        response = {
+            "slack_name": slack_name,
+            "current_day": current_day,
+            "utc_time": utc_time,
+            "track": track,
+            "github_file_url": "https://github.com/sapencio/HNGx/blob/master/stage%201/app.py",
+            "github_repo_url": "https://github.com/sapencio/HNGx/tree/master",
+            "status_code": 200
+        }
+
+        return jsonify(response)
+
+    except Exception as e:
+        return jsonify({
+            "error": str(e)
+        })
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
