e125dca28e5d8d4dc88d12106a8774445b3f1533
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2023-09-09 21:56:12 +0300

    Update syntax error

--- stage 1/app.py
+++ stage 1/app.py
@@ -1,8 +1,7 @@
-from flask import Flask, make_response
+from flask import Flask
 from datetime import datetime, timezone
 
 app = Flask(__name__)
-resp = make_response(200, 'OK')
 
 @app.route('/')
 def hello(slack_name: str, track: str = None):
@@ -13,5 +12,5 @@ def hello(slack_name: str, track: str = None):
         "track": track,
         "github_file_url": "https://github.com/sapencio/HNGx/blob/master/stage%201/app.py",
         "github_repo_url": "https://github.com/sapencio/HNGx/tree/master",
-        "status_code": resp,
+        "status_code": 200,
     }
